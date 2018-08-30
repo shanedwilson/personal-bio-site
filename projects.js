d1cf698751fc71482238e7d7f0eefda0b5e6b0da
Shane D Wilson <me@shanedwilson.com>
2018-08-29 22:21:01 -0500

    finished logic for available function

--- projects.js
+++ projects.js
@@ -1,6 +1,6 @@
 let projects = [
     {
-        title: "Cool Project", 
+        title: "Cool Project1", 
         screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
         description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
         technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
@@ -10,17 +10,17 @@ let projects = [
       },
 
       {
-        title: "Cool Project", 
+        title: "Cool Project2", 
         screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
         description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
         technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
-        available: true,
+        available: false,
         url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
         githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
       },
 
       {
-        title: "Cool Project", 
+        title: "Cool Project3", 
         screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
         description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
         technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
@@ -39,17 +39,19 @@ let newString = '';
 
 const createProjectCards = () => {
   for (let i = 0; i < projects.length; i++) {
-  newString += `<div class="project">`
-    newString += `<img src=${projects[i].screenshot}></img>`
-    newString +=`<div class="projectInfo"`
-      newString += `<h4>${projects[i].title}:</h4>`
-      newString += `<p>${projects[i].description}</p>`
-      newString += `<p>Technologies Used: ${projects[i].technologiesUsed}</p>`
-      newString += `<p><a href="${projects[i].url}">Project Url</a></p>`
-      newString += `<p><a href="${projects[i].githubUrl}">GitHub Url</a></p>`
-    newString += `</div>`  
-  newString += `</div>`; 
-  }
+    if (projects[i].available === true) {
+      newString += `<div class="project">`
+        newString += `<img src=${projects[i].screenshot}></img>`
+        newString +=`<div class="projectInfo"`
+          newString += `<h4>${projects[i].title}:</h4>`
+          newString += `<p>${projects[i].description}</p>`
+          newString += `<p>Technologies Used: ${projects[i].technologiesUsed}</p>`
+          newString += `<p><a href="${projects[i].url}">Project Url</a></p>`
+          newString += `<p><a href="${projects[i].githubUrl}">GitHub Url</a></p>`
+        newString += `</div>`  
+      newString += `</div>`;
+    };
+  } 
   printToDom(newString, 'projectsPage');
 };
 
