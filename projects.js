01fbbb1b4011d134f326130590c93bea45312b1e
Shane D Wilson <me@shanedwilson.com>
2018-08-29 22:01:28 -0500

    refined projects js

--- projects.js
+++ projects.js
@@ -1,1 +1,57 @@
-console.log('hi');
+let projects = [
+    {
+        title: "Cool Project", 
+        screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
+        description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+        technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+        available: true,
+        url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+        githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+      },
+
+      {
+        title: "Cool Project", 
+        screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
+        description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+        technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+        available: true,
+        url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+        githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+      },
+
+      {
+        title: "Cool Project", 
+        screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
+        description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+        technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+        available: true,
+        url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+        githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+      }
+];
+
+const printToDom = (stringToPrint, divId) => {
+  const selectedDiv = document.getElementById(divId);
+  selectedDiv.innerHTML += stringToPrint;
+};
+
+let newString = '';
+
+const createProjectCards = () => {
+  for (let i = 0; i < projects.length; i++) {
+  newString += `<div class="project">`
+    newString += `<img src=${projects[i].screenshot}></img>`
+    newString +=`<div class="projectInfo"`
+      newString += `<h4>${projects[i].title}:</h4>`
+      newString += `<p>${projects[i].description}</p>`
+      newString += `<p>Technologies Used: ${projects[i].technologiesUsed}</p>`
+      newString += `<p><a href="${projects[i].url}">Project Url</a></p>`
+      newString += `<p><a href="${projects[i].githubUrl}">GitHub Url</a></p>`
+    newString += `</div>`  
+  newString += `</div>`; 
+  }
+  printToDom(newString, 'projectsPage');
+};
+
+createProjectCards();
+
