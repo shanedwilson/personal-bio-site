122e760d364b375401f5e3955ac6f85041b4bb2f
Shane D Wilson <me@shanedwilson.com>
2018-11-03 18:44:12 -0500

    wip

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import './index.scss';
+import sdwPhoto from './images/NewSDW.jpg';
 import projects from './javascripts/components/projects';
 import events from './javascripts/helpers/events';
 import tech from './javascripts/components/tech';
+
+$('#bio-pic').attr('src', sdwPhoto);
+
 
 const initApp = () => {
   events.setEvents();
