13f91400fbd368729b5df5ac3fdade4cec02e896
Shane D Wilson <me@shanedwilson.com>
2018-11-05 09:02:14 -0600

    added footer icons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import $ from 'jquery';
 import 'bootstrap';
 import './index.scss';
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import {
+  faTwitter, faGithub, faLinkedin, faInstagram,
+} from '@fortawesome/free-brands-svg-icons';
+import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons';
 import sdwPhoto from './images/NewSDW.jpg';
 import projects from './javascripts/components/projects';
 import events from './javascripts/helpers/events';
@@ -14,6 +19,8 @@ const initApp = () => {
   projects.initView();
   projects.getProjects();
   tech.getTech();
+  library.add(faTwitter, faGithub, faLinkedin, faInstagram, faEnvelopeSquare);
+  dom.watch();
 };
 
 initApp();
