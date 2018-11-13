9a6f13e1cd7b88f2590fe90599b3440e067664a6
Shane D Wilson <me@shanedwilson.com>
2018-11-12 18:12:46 -0600

    2 real projects

--- src/javascripts/components/projects.js
+++ src/javascripts/components/projects.js
@@ -14,16 +14,15 @@ const createProjectCards = (projects) => {
   projects.forEach((project) => {
     if (project.available === true) {
       newString += `
-      <div class="project card flex-row flex-wrap mb-4" id="${project.id}">
+      <div class="project card mt-3" id="${project.id}">
         <div class="card-header border-0">
           <img class="card-img-top img-thumbnail img-fluid" src="${project.screenshot}" alt="${project.title}">
         </div>
-        <div class="card-block px-2">
+        <div class="card-block">
           <h4 class="card-title text-center">${project.title}</h4>
-          <p class="card-text">${project.description}</p>
-          <p class="card-text">Technologies Used: ${project.technologiesUsed}</p>
+          <p class="card-text text-center">${project.description}</p>
+          <p class="card-text text-center">Technologies Used: ${project.technologiesUsed}</p>
         </div> 
-        <div class="w-100"></div> 
         <div class="card-footer w-100 text-muted row d-flex justify-content-around">
             <p class="text-center"><a href='${project.url}' target="_blank" class="card-link">Project Url</a></p>
             <p class="text-center"><a href='${project.githubUrl}' target="_blank" class="card-link">GitHub Url</a></p>
